a3e3c068a37e566738ecb5324408b7cc4a36fec0
donallen <da34773@ravens.racc.edu>
2018-04-05 16:35:19 -0400

    added click event functions and listeners. Added preloader for home page on initial load. Updated the links in the menu and logo to call page content from our WordPress API.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,11 @@
-// JavaScript Document
+// inside out project STEP-105
 
 window.onload = init();
 
 function init() {
 
 	window.addEventListener('scroll', function (e) {
-			var distanceY = window.pageYOffset || document.documentElement.scrollTop,
+		var distanceY = window.pageYOffset || document.documentElement.scrollTop,
 			shrinkOn = 300,
 			header = document.querySelector("header");
 		if (distanceY > shrinkOn) {
@@ -23,56 +23,62 @@ function init() {
 		url: 'https://me.donaldallenjr.com/wp-json/wp-api-menus/v2/menus/2',
 		dataType: 'json',
 		success: function (data) {
-			
 			$('nav').hide();
-			
 			var menu = menuBuilder(data.items);
-			
 			$('nav').html(menu).slideDown();
-			
-			// $('nav').append(menu);
+			$('nav li a').click(function() {
+				getPage($(this).data("pgid"));
+			});
+			getPage(65);
 			$("#loaderDiv").fadeOut("slow");
-			
 		},
-		   error: function () {
-			   
-		   console.log('all is not good');
-			   
+		error: function () {
+			console.log('all is not good');
 		}
 	});
-
 }
 
 
 function menuBuilder(obj) {
-
 	var theMenu = '';
-	
 	if (obj.length > 0) {
-		
 		theMenu = theMenu + '<ul>';
-		
 		obj.forEach(function (item) {
-			
-			theMenu = theMenu + '<li><a href="#">' + item.title + '</a>';
-			
+			theMenu = theMenu + '<li><a href="#" data-pgid="' + item.object_id + '">' + item.title + '</a>';
 			if (item.children) {
-			
 				theMenu = theMenu + menuBuilder(item.children);
-				
 			}
-			
 			theMenu = theMenu + '</li>';
-			
 		});
-		
 		theMenu = theMenu + '</ul>';
-							
-	}else{
-		
+	} else {
 		console.log('no data');
-		
 	}
-	
 	return theMenu;
+}
+
+function getPage(obj) {
+	$("#loaderDiv").fadeIn("slow");
+	$.ajax({
+		method: 'GET',
+		url: 'https://me.donaldallenjr.com/wp-json/wp/v2/pages/' + obj,
+		dataType: 'json',
+		success: function (data) {
+			var pgbuild = '';
+			pgbuild = '<section><div class="container">' + data.content.rendered + '</div></section>';
+			$("#content").fadeOut(function () {
+				$('html').animate({
+					scrollTop: 0
+				}, 'slow'); //IE, FF
+				$('body').animate({
+					scrollTop: 0
+				}, 'slow'); //chrome, don'tknow if Safari works
+				$(this).html(pgbuild).fadeIn();
+				$("#loaderDiv").fadeOut("slow");
+			});
+		},
+		error: function () {
+			console.log('bad');
+		}
+	});
 }
